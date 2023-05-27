5b9f4b855c17f83d986fa28aa5d4a9887b73bf29
ddpo <anonymized.ddpo@gmail.com>
2023-05-27 01:05:45 -0700

    minor edits to readme and launch files

--- pipeline/run-sparse.sh
+++ pipeline/run-sparse.sh
@@ -5,7 +5,6 @@ for i in $(seq 0 9);
 do
     echo Iteration: $i
 
-    # if [ $i -gt 0 ]; then
     ## collect reference unfiltered dataset for evaluation statistics
     python pipeline/sample.py --dataset ${dataset} --iteration ${i} \
             --max_samples 10000 --mask_param 0 \
@@ -22,15 +21,16 @@ do
     JAX_PLATFORMS=cpu python pipeline/save_sizes.py \
         --dataset ${dataset} --iteration ${i} \
         --loadpath samples/${i}
-    # fi
 
     ## finetune
     python pipeline/finetune.py --dataset ${dataset} --iteration ${i}
+
     ## rm logs folder with local model checkpoints
     sleep 1m
     rm -rf logs
 done
 
+## save samples from last model
 i=$(($i+1))
 
 python pipeline/sample.py --dataset ${dataset} --iteration ${i} \
